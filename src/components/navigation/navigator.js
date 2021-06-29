94810c7e9e8a8ad01859986f48906bf2ed5eea74
Benjamin RAULT <b_rault@outlook.fr>
2021-06-29 12:00:42 +0200

    Implémentation de la navigation et du theming

---
+++ src/components/navigation/navigator.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+
+import HomeStack from "../home/homeStackScreen";
+import SearchScreen from "../search/searchScreen";
+import GeoScreen from "../localisation/geoScreen";
+import SettingScreen from "../settings/settingsScreen";
+
+import { useTheme } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+const DEFAULT_ICON_SIZE = 26
+
+export default function Navigator() {
+	const Tab = createMaterialBottomTabNavigator();
+    const theme = useTheme();
+
+	return (
+		<NavigationContainer>
+			<Tab.Navigator initialRouteName="home" labeled={false} barStyle={{backgroundColor:theme.colors.primary}}>
+				<Tab.Screen
+					name="home"
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Icon name="home" size={DEFAULT_ICON_SIZE} color={color} />
+						),
+					}}
+				>
+					{() => <HomeStack />}
+				</Tab.Screen>
+				<Tab.Screen
+					name="search"
+					options={{
+						tabBarIcon: ({ color, focused }) => (
+							<Icon name="magnify" size={DEFAULT_ICON_SIZE} color={color} style={{}}/>
+						),
+					}}
+				>
+					{() => <SearchScreen />}
+				</Tab.Screen>
+				<Tab.Screen
+					name="loc"
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Icon name="map-marker" size={DEFAULT_ICON_SIZE} color={color} />
+						),
+					}}
+				>
+					{() => <GeoScreen />}
+				</Tab.Screen>
+				<Tab.Screen
+					name="settings"
+					options={{
+						tabBarIcon: ({ color }) => (
+							<Icon name="cog" size={DEFAULT_ICON_SIZE} color={color} />
+						),
+					}}
+				>
+					{() => <SettingScreen />}
+				</Tab.Screen>
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
