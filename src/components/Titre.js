8edfa446c543f6e4679143ebf8c8ae4613b97bb0
Benjamin RAULT <b_rault@outlook.fr>
2021-07-01 15:37:31 +0200

    Modification du titre de l'application

--- src/components/Titre.js
+++ src/components/Titre.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import {
-    StatusBar, StyleSheet, Dimensions, Text, View, TouchableOpacity
-} from 'react-native';
-import { Title } from 'react-native-paper';
+import React from "react";
+import { Text, View } from "react-native";
 
 const Tit = () => {
-
-    return (
-        <View style={{ alignItems: 'center' }}>
-            <Text style={{ marginTop: 15, color: '#5FC2BA', fontSize: 45, fontFamily: 'Roboto' }}>Art Breaker</Text>
-        </View>
-    )
-}
+	return (
+		<View style={{ alignItems: "center" }}>
+			<Text
+				style={{
+					marginTop: 15,
+					color: "#5FC2BA",
+					fontSize: 45,
+					fontFamily: "Roboto",
+				}}
+			>
+				QultureG
+			</Text>
+		</View>
+	);
+};
 
 export default Tit;
