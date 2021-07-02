f509f3c7185260628cd79fe1811f31b85b5fd06f
Benjamin RAULT <b_rault@outlook.fr>
2021-07-02 10:51:57 +0200

    Ajout de la fonction pour ajouter un fake Artist

--- src/data/schemas/artistSchema.js
+++ src/data/schemas/artistSchema.js
@@ -1,4 +1,4 @@
-import { ObjectID } from "bson";
+import { ObjectId } from "bson";
 
 export const artistSchema = {
 	name: "Artist",
@@ -60,4 +60,24 @@ export function deleteArtistByID(realm, artistID){
     })
     artist = null
     console.log("artistSchema::deleteArtistByID>Suppression d'un artiste");
+}
+
+// ======================================== FAKE ENTRIES =============================================
+
+export function addDummyArtist(realm){
+	const dummy = {
+		_id:ObjectId(),
+		groupname:"AC/DC",
+		bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam egestas pretium elit sit amet tristique. Praesent posuere urna mi, ut gravida purus consequat et. Donec mattis faucibus magna, a commodo lectus vulputate vel. Sed ac felis commodo, aliquam enim ac, hendrerit augue. Fusce ullamcorper a dolor ornare molestie. Donec sit amet turpis vulputate, fringilla turpis eget, ullamcorper augue. Proin ac diam sed sapien faucibus pretium. Morbi varius efficitur vulputate. Fusce consectetur fringilla justo, vitae tincidunt magna varius vitae. Morbi nec ullamcorper erat"
+		+"Etiam id posuere nisl, sit amet ultrices sem. Nullam dapibus sollicitudin nisi, eu iaculis erat faucibus sagittis. Praesent iaculis velit congue tempor tempus. Mauris ut dapibus ex, at lacinia felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse aliquam felis metus. Mauris scelerisque lectus metus, quis suscipit erat elementum ut."
+		+"Duis bibendum metus ante, in mollis elit fermentum vel. Sed pulvinar in leo et pulvinar. In hac habitasse platea dictumst. Donec a neque fermentum, semper enim lobortis, ornare justo. Nulla facilisi. Pellentesque ac nibh libero. Nam elit eros, sodales in viverra quis, semper maximus elit. In hac habitasse platea dictumst. Vestibulum sit amet vulputate lacus. Donec et posuere neque. Maecenas sollicitudin malesuada mi, ut fermentum risus cursus vitae."
+		+"Nam lacus lectus, volutpat eget ornare sit amet, dictum non nulla. Etiam vel ex at velit pellentesque porta. Aliquam semper libero purus, vitae tempor ex efficitur at. Cras in arcu ac purus consectetur molestie. Ut vulputate nisi turpis, eget hendrerit ex tristique ut. Proin et orci viverra, efficitur metus vel, eleifend tortor. Fusce id nibh non ex eleifend volutpat. Etiam venenatis velit dolor, vel tempor elit suscipit sed. Duis rhoncus vulputate justo, in viverra turpis blandit ac. Vestibulum posuere, elit sed tempor auctor, nulla quam ornare dolor, a aliquet purus risus vel leo. Curabitur quis iaculis mi, eu accumsan mauris."
+		+"Maecenas turpis urna, aliquam a consequat at, cursus eget ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam sagittis at ligula tincidunt euismod. Integer vehicula varius metus, sed finibus est bibendum sed. Fusce urna justo, laoreet sed eleifend ac, rutrum eu orci. Aliquam vehicula elementum mi, in lobortis dui egestas id. Aliquam pulvinar convallis iaculis. Proin commodo, magna egestas tristique venenatis, metus neque rutrum ex, vel ullamcorper justo velit id ante. Aenean efficitur egestas nunc, eu euismod elit fermentum eleifend. Suspendisse vitae aliquam sapien, sed scelerisque ligula. Nullam sollicitudin ante leo, vel rutrum justo dapibus eget. Suspendisse potenti. In tincidunt vehicula nisi sit amet blandit.",
+		email:"acdc.pro@outlook.com",
+		password:"pwd",
+		type:[{category:"Musique", name="Rock"}]
+	}
+	realm.write(()=>{
+		createArtist(realm, dummy)
+	})
 }
