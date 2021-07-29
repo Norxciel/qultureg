86df3e49201ed4e3ec7dacbcddf28ff2fd79f456
Benjamin RAULT <b_rault@outlook.fr>
2021-07-29 10:54:09 +0200

    Début de modification de la searchBar

--- src/components/search/searchScreen.js
+++ src/components/search/searchScreen.js
@@ -6,232 +6,334 @@ import {
 	SafeAreaView,
 	ImageBackground,
 	Image,
-	TouchableOpacity
+	TouchableOpacity,
+	FlatList,
 } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
+
+import { Searchbar } from "react-native-paper";
+
 import { themes } from "../themes/themes";
 
 // import components
 import SearchBar from "./searchBar";
 
 const Search = () => {
+	const [data, setData] = React.useState([]);
+
+	const resetData = () => {
+		setData([]);
+	};
+
+	const [searchQuery, setSearchQuery] = React.useState("");
+	const onChangeSearch = (query) => setSearchQuery(query);
+
+	React.useEffect(() => {
+		// Effect
+		return () => {
+			// Clean Up
+		};
+	}, [searchQuery]);
+
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: "#1C2942" }}>
-			<View style={styles.main_container}>
-				<View style={styles.containerSearch}>
-					<SearchBar />
+			{data.length != 0 ? (
+				<FlatList
+					data={data}
+					keyExtractor={(item, id) => id}
+					renderItem={({ item }) => {
+						return <Text>Bonjour</Text>;
+					}}
+				/>
+			) : (
+				<View style={styles.main_container}>
+					<View style={styles.containerSearch}>
+						<Searchbar
+							placeholder="Recherche..."
+							onChangeText={onChangeSearch}
+							value={searchQuery}
+							style={{
+								width: "90%",
+							}}
+						/>
+					</View>
+
+					{/* Container principal  */}
+					<View style={styles.containerResult}>
+						{/* Container 1 */}
+						<View
+							style={{
+								width: "100%",
+								height: "25%",
+								flexDirection: "row",
+							}}
+						>
+							{/* Bloc 1 left */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/musique/musique1.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 60,
+									}}
+								>
+									MUSIQUE
+								</Text>
+							</TouchableOpacity>
+
+							{/* bloc 2 right */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/sculpture/Sculpture1.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 60,
+									}}
+								>
+									SCULTURE
+								</Text>
+							</TouchableOpacity>
+						</View>
+						{/* Container 2 */}
+						<View
+							style={{
+								width: "100%",
+								height: "25%",
+								flexDirection: "row",
+							}}
+						>
+							{/* Bloc 1 left */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/litterature/litterature1.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 50,
+									}}
+								>
+									LITTÉRATURE
+								</Text>
+							</TouchableOpacity>
+
+							{/* bloc 2 right */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/peinture/peinture5.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 60,
+									}}
+								>
+									PEINTURE
+								</Text>
+							</TouchableOpacity>
+						</View>
+						{/* Container 3 */}
+						<View
+							style={{
+								width: "100%",
+								height: "25%",
+								flexDirection: "row",
+							}}
+						>
+							{/* Bloc 1 left */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/Histoire/histoire5.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 60,
+									}}
+								>
+									HISTOIRE
+								</Text>
+							</TouchableOpacity>
+
+							{/* bloc 2 right */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/Theatre/Theatre4.jpeg")}
+									resizeMode="center"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 63,
+									}}
+								>
+									THÉÂTRE
+								</Text>
+							</TouchableOpacity>
+						</View>
+						{/* Container 1 */}
+						<View
+							style={{
+								width: "100%",
+								height: "25%",
+								flexDirection: "row",
+							}}
+						>
+							{/* Bloc 1 left */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/Archéologie/Archeo3.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 50,
+									}}
+								>
+									ARCHÉOLOGIE
+								</Text>
+							</TouchableOpacity>
+
+							{/* bloc 2 right */}
+							<TouchableOpacity
+								style={{
+									width: "50%",
+									height: "100%",
+									alignItems: "center",
+								}}
+							>
+								<Image
+									source={require("../../assets/images/litterature/litterature1.jpeg")}
+									resizeMode="contain"
+									style={{
+										width: "90%",
+										height: "100%",
+										opacity: 0.6,
+									}}
+								/>
+								<Text
+									style={{
+										fontWeight: "bold",
+										color: "white",
+										position: "absolute", // child
+										bottom: 60, // position where you want
+										left: 50,
+									}}
+								>
+									LITTÉRATURE
+								</Text>
+							</TouchableOpacity>
+						</View>
+					</View>
 				</View>
-
-				{/* Container principal  */}
-				<View style={styles.containerResult}>
-
-					{/* Container 1 */}
-					<View style={{width: '100%', height: '25%', flexDirection: 'row'}}>
-
-						{/* Bloc 1 left */}
-						<TouchableOpacity style={{ width: '50%', height: '100%',alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/musique/musique1.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 60
-								}}
-							>
-								MUSIQUE
-							</Text>
-						</TouchableOpacity>
-
-						{/* bloc 2 right */}
-						<TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/sculpture/Sculpture1.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 60
-								}}
-							>
-								SCULTURE
-							</Text>
-						</TouchableOpacity>
-					</View>
-					{/* Container 2 */}
-					<View style={{width: '100%', height: '25%', flexDirection: 'row'}}>
-
-						{/* Bloc 1 left */}
-						<TouchableOpacity style={{ width: '50%', height: '100%',alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/litterature/litterature1.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 50
-								}}
-							>
-								LITTÉRATURE
-							</Text>
-						</TouchableOpacity>
-
-						{/* bloc 2 right */}
-						<TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/peinture/peinture5.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 60
-								}}
-							>
-								PEINTURE
-							</Text>
-						</TouchableOpacity>
-					</View>
-					{/* Container 3 */}
-					<View style={{width: '100%', height: '25%', flexDirection: 'row'}}>
-
-						{/* Bloc 1 left */}
-						<TouchableOpacity style={{ width: '50%', height: '100%',alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/Histoire/histoire5.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 60
-								}}
-							>
-								HISTOIRE
-							</Text>
-						</TouchableOpacity>
-
-						{/* bloc 2 right */}
-						<TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/Theatre/Theatre4.jpeg')}
-								resizeMode= 'center'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6,
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 63
-								}}
-							>
-								THÉÂTRE
-							</Text>
-						</TouchableOpacity>
-					</View>
-					{/* Container 1 */}
-					<View style={{width: '100%', height: '25%', flexDirection: 'row'}}>
-
-						{/* Bloc 1 left */}
-						<TouchableOpacity style={{ width: '50%', height: '100%',alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/Archéologie/Archeo3.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 50
-								}}
-							>
-								ARCHÉOLOGIE
-							</Text>
-						</TouchableOpacity>
-
-						{/* bloc 2 right */}
-						<TouchableOpacity style={{ width: '50%', height: '100%', alignItems: 'center'}}>
-							<Image 
-								source={require('../../assets/images/litterature/litterature1.jpeg')}
-								resizeMode= 'contain'
-								style={{
-									width: '90%',
-									height: '100%',
-									opacity: 0.6
-								}}
-							/>
-							<Text
-								style={{
-								fontWeight: 'bold',
-								color: 'white',
-								position: 'absolute', // child
-								bottom: 60, // position where you want
-								left: 50
-								}}
-							>
-								LITTÉRATURE
-							</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
-				
-			</View>
+			)}
 		</SafeAreaView>
 	);
 };
